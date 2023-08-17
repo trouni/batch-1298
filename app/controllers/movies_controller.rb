ae920877b044495b3edf332149960e2ae5dfab9b
Trouni Tiet <trouni@gmail.com>
2023-08-17 10:48:07 +0900

    Add multiple filters instructions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,27 @@
 class MoviesController < ApplicationController
   def index
+    # # --------
+    # # Implementation with multiple filters (example only, doesn't work with the current app)
+    # # --------
+
+    # @movies = Movie.all
+
+    # # Price filter
+    # @movies = @movies.where('price >= ?', params[:min_price]) if params[:min_price].present?
+    # @movies = @movies.where('price <= ?', params[:max_price]) if params[:max_price].present?
+
+    # # Size filter
+    # @movies = @movies.where(size: params[:size]) if params[:size].present?
+
+    # # Location filter
+    # @movies = @movies.near(params[:location]) if params[:location].present?
+
+    # # Content filter with PG Search
+    # @movies = @movies.global_search(params[:q])
+
     @movies =
       if params[:q].present?
+        # PG Search Implementation
         Movie.global_search(params[:q])
 
         # # SQL Implementation
